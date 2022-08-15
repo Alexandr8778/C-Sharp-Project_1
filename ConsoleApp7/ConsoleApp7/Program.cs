d66dc5898cde0bada1af3f3f095c029f07986e54
Alexander <109409789+Alexandr8778@users.noreply.github.com>
2022-08-16 00:55:02 +0500

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите трехзначное число: ");
+            string s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            if (n == 6 || n == 7)
+            {
+                Console.WriteLine("Да, это выходной.");
+            }
+            else if (n > 6)
+            {
+                Console.WriteLine("Такого дня в недели не существует!");
+            }
+            else Console.WriteLine("Нет, это будний день!");
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+
+}
