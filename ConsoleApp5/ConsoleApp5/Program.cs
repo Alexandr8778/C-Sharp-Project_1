ea4204649278af9ec2cf24ed714082f48479ea26
Alexander <109409789+Alexandr8778@users.noreply.github.com>
2022-08-16 00:53:34 +0500

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string s = 782.ToString();
+            Console.WriteLine(s);
+            Console.WriteLine("Вторым числом является: " + s[1]);
+
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
